2a51644145170befb862237d6aad9a0d29c9c801
GeekSK07 <143426700+GeekSK07@users.noreply.github.com>
2023-09-05 20:34:13 +0530

    852. Peak Index in a Mountain Array.java

--- 852. Peak Index in a Mountain Array.java
+++ 852. Peak Index in a Mountain Array.java
@@ -1,11 +1,15 @@
 class Solution {
     public int peakIndexInMountainArray(int[] arr) {
-    int max=0;
-    for(int i=1;i<arr.length;i++){
-        if(arr[i]>arr[max]){
-            max=i;
-        }
-    }
-    return max;
+   int start=0,end=arr.length-1;
+   while(start<end){
+       int mid=start + (end-start)/2;
+       if(arr[mid]>arr[mid+1]){
+           end=mid;
+       }
+       else{
+           start=mid+1;
+       }
+   }
+   return start;
     }
 }
